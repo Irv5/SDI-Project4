f340b42f4821197564d82ccb816ce05215fcad4e
Irvin Reaves <irv5@Irvins-MacBook-Pro.local>
2012-03-22 11:54:27 -0400

    Added the bPhoneNumberCheck function to the SDI-Project4 Repository

--- sdi-project4.js
+++ sdi-project4.js
@@ -13,8 +13,27 @@ var nDecPlaceAdded = function (number) {
 	return (number + " rounded to " + numberWithDec);	
 }
 
+// Boolean: Does a string follow a 123-456-7890 pattern like a phone number?
+
+var bPhoneNumberCheck = function (phoneNumber) {
+	var number = phoneNumber,
+		regExpPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/, // This regExpPattern allowes 10 digits with or without hyphens.
+		check;
+
+	check = regExpPattern.test(number);
+
+	if (check) {
+		return (true + " valid phone number.");
+	}
+	else {
+		return (false + " not a valid phone number.");
+	}
+}
 
 
 	
 	console.log(nDecPlaceAdded(3.6));
 	console.log(nDecPlaceAdded(9.9));
+	
+	console.log(bPhoneNumberCheck("314-583-0268"));
+	console.log(bPhoneNumberCheck("7046379381"));
